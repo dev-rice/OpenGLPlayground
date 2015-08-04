343d434db4135fe62ffd233b0323c833502cb398
Chris Rice <chrisrice93@gmail.com>
2015-08-04 17:01:32 -0600

    added some simple plane drawing code.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,24 @@
 
 using namespace std;
 
+// Shader sources
+const GLchar* vertexSource =
+    "#version 150 core\n"
+    "in vec2 position;"
+    "in vec3 color;"
+    "out vec3 Color;"
+    "void main() {"
+    "   Color = color;"
+    "   gl_Position = vec4(position, 0.0, 1.0);"
+    "}";
+const GLchar* fragmentSource =
+    "#version 150 core\n"
+    "in vec3 Color;"
+    "out vec4 outColor;"
+    "void main() {"
+    "   outColor = vec4(Color, 1.0);"
+    "}";
+
 char getKeyboardInputCharacter(SDL_Event& event) {
 
     char key = 0;
@@ -26,20 +44,22 @@ void handleInputs(Mouse& mouse, Window& window) {
 
     SDL_Event event;
     SDL_PollEvent(&event);
-    if (event.type == SDL_QUIT) {
-        window.requestClose();
-    }
 
+    char key = getKeyboardInputCharacter(event);
 
     // SDL_Event event;
     // SDL_PollEvent(&event);
     //
     // SDL_Scancode key_scancode = event.key.keysym.scancode;
-    char key = getKeyboardInputCharacter(event);
-    if (key == 'm'){
-        mouse.hide();
-    } else if (key == 'c') {
-        mouse.centerInWindow();
+    if (event.type == SDL_QUIT) {
+        window.requestClose();
+    }
+    if (key != 0) {
+        if (key == 'm'){
+            mouse.hide();
+        } else if (key == 'c') {
+            mouse.centerInWindow();
+        }
     }
 }
 
@@ -49,10 +69,77 @@ int main(int argc, char* argv[]) {
     OpenGLContext gl_context(4, 1, window);
     Mouse mouse(window);
 
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    GLfloat vertices[] = {
+        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create an element array
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+
+    GLuint elements[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    // Create Vertex Array Object
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+
+    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+    glEnableVertexAttribArray(colAttrib);
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+
     // Display loop
     while(window.isOpen()) {
+
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a rectangle from the 2 triangles using 6 indices
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         handleInputs(mouse, window);
         window.display();
+
     }
 
     // Close the window
