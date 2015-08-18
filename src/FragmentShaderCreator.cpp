604e108aa3c2ce2c5a6be5acbb37b83b05a92136
Chris Rice <chrisrice93@gmail.com>
2015-08-18 08:53:25 -0600

    Removed print statements

--- src/FragmentShaderCreator.cpp
+++ src/FragmentShaderCreator.cpp
@@ -1,6 +1,5 @@
 #include "FragmentShaderCreator.hpp"
 
 GLuint FragmentShaderCreator::create() {
-    cout << "Creating Fragment Shader\n";
     return glCreateShader(GL_FRAGMENT_SHADER);
 }
--- src/VertexShaderCreator.cpp
+++ src/VertexShaderCreator.cpp
@@ -1,6 +1,5 @@
 #include "VertexShaderCreator.hpp"
 
 GLuint VertexShaderCreator::create() {
-    cout << "Creating Vertex Shader\n";
     return glCreateShader(GL_VERTEX_SHADER);
 }
